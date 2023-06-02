21280ee25ca20a536b31c8d1cd12862fb441fcae
James Ogilvie <jameso.ogilvie@gmail.com>
2023-06-02 17:07:43 +1000

    Added oh my zsh

--- post-install.sh
+++ post-install.sh
@@ -6,5 +6,8 @@ sed -i 's/dmenu_run/rofi -show run/' ~/.config/i3/config
 # Apply Xresources
 sudo xrdb ~/.Xresources
 
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Reboot 
 sudo reboot now
